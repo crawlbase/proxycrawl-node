418c95ec6ba623285f85e1cc61e6d069e6ec07ec
proxycrawl <info@proxycrawl.com>
2020-03-22 18:14:24 +0800

    Extract base from crawling api

---
+++ src/base-api.js
@@ -0,0 +1,119 @@
+const https = require('https');
+const zlib = require('zlib');
+const { URL } = require('url');
+const { defaults } = require('./config.js');
+const { snakeCase, lowerHeaders } = require('./utils.js');
+
+const _APIURL_ = 'https://api.proxycrawl.com/';
+
+/**
+ * The BaseAPI class which does the calls to the ProxyCrawlAPI itself.
+ *
+ * Copyright ProxyCrawl
+ * Licensed under the Apache License 2.0
+ */
+class BaseAPI {
+
+  constructor(options) {
+    if (undefined === options.token || '' === options.token) {
+      throw new Error('Token is required to use the API, please pass token option');
+    }
+
+    this.options = options;
+    this.options.timeout = this.options.timeout || defaults.timeout;
+  }
+
+  request(path, options = {}) {
+    const url = this.buildURL(path, options);
+
+    const headers = {
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Encoding': 'gzip,deflate'
+    };
+    if (('POST' === options.method || 'PUT' === options.method) && '' !== options.postData) {
+      headers['Content-Type'] = options.postContentType || defaults.postContentType;
+      headers['Content-Length'] = Buffer.byteLength(options.postData);
+    }
+    const requestOptions = {
+      method: options.method,
+      host: url.host,
+      path: url.pathname + url.search,
+      headers
+    };
+    return new Promise((resolve, reject) => {
+      const request = https.request(requestOptions, (response) => this.processResponse(response).then(resolve).catch(reject));
+      request.setTimeout(this.options.timeout, () => request.destroy('Request timeout'));
+      request.on('error', reject);
+      if (('POST' === options.method || 'PUT' === options.method) && '' !== options.postData) {
+        request.write(options.postData);
+      }
+      request.end();
+    });
+  }
+
+  processResponse(response) {
+    response.headers = lowerHeaders(response.headers);
+    response.originalStatus = response.headers.original_status * 1;
+    response.pcStatus = response.headers.pc_status * 1;
+    response.url = response.headers.url;
+
+    return new Promise((resolve, reject) => {
+      let encoding = response.headers['content-encoding'];
+      if ('gzip' === encoding || 'deflate' === encoding) {
+        const pipe = zlib[encoding === 'gzip' ? 'createGunzip' : 'createInflate']();
+        const buffer = [];
+        response.pipe(pipe);
+        pipe.on('data', (data) => buffer.push(data.toString()));
+        pipe.on('end', () => {
+          response.body = buffer.join('');
+          if (response.headers['content-type'] && response.headers['content-type'].indexOf('json') > -1) {
+            response.json = JSON.parse(response.body);
+            response.originalStatus = response.json.original_status * 1;
+            response.pcStatus = response.json.pc_status * 1;
+            response.url = response.json.url;
+          }
+          return resolve(response);
+        });
+        pipe.on('error', (error) => {
+          response.statusCode = 400;
+          response.body = 'Error unzipping response';
+          return reject(error);
+        });
+      } else {
+        let rawData = '';
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => rawData += chunk);
+        response.on('end', () => {
+          response.body = rawData;
+          if (response.headers['content-type'] && response.headers['content-type'].indexOf('json') > -1) {
+            response.json = JSON.parse(response.body);
+            response.originalStatus = response.json.original_status * 1;
+            response.pcStatus = response.json.pc_status * 1;
+            response.url = response.json.url;
+          }
+          return resolve(response);
+        });
+      }
+    });
+  }
+
+  buildURL(path, options) {
+    let url = _APIURL_ + path + '?token=' + this.options.token;
+
+    for (const [key, value] of Object.entries(options)) {
+      if ('method' === key) {
+        continue;
+      }
+      url += '&' + snakeCase(key) + '=' + encodeURIComponent(value);
+    }
+
+    if (undefined === URL) { // Fix for older node versions
+      const urlModule = require('url');
+      return urlModule.parse(url);
+    }
+    return new URL(url);
+  }
+
+}
+
+module.exports = BaseAPI;
--- src/crawling-api.js
+++ src/crawling-api.js
@@ -1,11 +1,5 @@
-const https = require('https');
-const zlib = require('zlib');
 const querystring = require('querystring');
-const { URL } = require('url');
-const { defaults } = require('./config.js');
-const { snakeCase, lowerHeaders } = require('./utils.js');
-
-const _APIURL_ = 'https://api.proxycrawl.com/';
+const BaseAPI = require('./base-api.js');
 
 /**
  * A node class that acts as wrapper for ProxyCrawl Crawling API.
@@ -15,23 +9,16 @@ const _APIURL_ = 'https://api.proxycrawl.com/';
  * Copyright ProxyCrawl
  * Licensed under the Apache License 2.0
  */
-class CrawlingAPI {
-
-  constructor(options) {
-    if (undefined === options.token || '' === options.token) {
-      return console.error('Token is required to use the API, please pass token option');
-    }
-
-    this.options = options;
-    this.options.timeout = this.options.timeout || defaults.timeout;
-  }
+class CrawlingAPI extends BaseAPI {
 
   get(url, options = {}) {
+    options.url = url;
     options.method = 'GET';
-    return this.request(url, options);
+    return this.request('', options);
   }
 
   post(url, data, options = {}) {
+    options.url = url;
     options.method = options.method || 'POST';
     if ('object' === typeof data && undefined !== options.postType && 'json' === options.postType) {
       data = JSON.stringify(data);
@@ -40,104 +27,13 @@ class CrawlingAPI {
       data = querystring.stringify(data);
     }
     options.postData = data;
-    return this.request(url, options);
+    return this.request('', options);
   }
 
   put(url, data, options = {}) {
+    options.url = url;
     options.method = 'PUT';
     return this.post(url, data, options);
-  }
-
-  request(url, options = {}) {
-    url = this.buildURL(url, options);
-
-    const headers = {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Encoding': 'gzip,deflate'
-    };
-    if (('POST' === options.method || 'PUT' === options.method) && '' !== options.postData) {
-      headers['Content-Type'] = options.postContentType || defaults.postContentType;
-      headers['Content-Length'] = Buffer.byteLength(options.postData);
-    }
-    const requestOptions = {
-      method: options.method,
-      host: url.host,
-      path: url.pathname + url.search,
-      headers
-    };
-    return new Promise((resolve, reject) => {
-      const request = https.request(requestOptions, (response) => this.processResponse(response).then(resolve).catch(reject));
-      request.setTimeout(this.options.timeout, () => request.destroy('Request timeout'));
-      request.on('error', reject);
-      if (('POST' === options.method || 'PUT' === options.method) && '' !== options.postData) {
-        request.write(options.postData);
-      }
-      request.end();
-    });
-  }
-
-  processResponse(response) {
-    response.headers = lowerHeaders(response.headers);
-    response.originalStatus = response.headers.original_status * 1;
-    response.pcStatus = response.headers.pc_status * 1;
-    response.url = response.headers.url;
-
-    return new Promise((resolve, reject) => {
-      let encoding = response.headers['content-encoding'];
-      if ('gzip' === encoding || 'deflate' === encoding) {
-        const pipe = zlib[encoding === 'gzip' ? 'createGunzip' : 'createInflate']();
-        const buffer = [];
-        response.pipe(pipe);
-        pipe.on('data', (data) => buffer.push(data.toString()));
-        pipe.on('end', () => {
-          response.body = buffer.join('');
-          if (response.headers['content-type'] && response.headers['content-type'].indexOf('json') > -1) {
-            response.json = JSON.parse(response.body);
-            response.originalStatus = response.json.original_status * 1;
-            response.pcStatus = response.json.pc_status * 1;
-            response.url = response.json.url;
-          }
-          return resolve(response);
-        });
-        pipe.on('error', (error) => {
-          response.statusCode = 400;
-          response.body = 'Error unzipping response';
-          return reject(error);
-        });
-      } else {
-        let rawData = '';
-        response.setEncoding('utf8');
-        response.on('data', (chunk) => rawData += chunk);
-        response.on('end', () => {
-          response.body = rawData;
-          if (response.headers['content-type'] && response.headers['content-type'].indexOf('json') > -1) {
-            response.json = JSON.parse(response.body);
-            response.originalStatus = response.json.original_status * 1;
-            response.pcStatus = response.json.pc_status * 1;
-            response.url = response.json.url;
-          }
-          return resolve(response);
-        });
-      }
-    });
-  }
-
-  buildURL(url, options) {
-    url = encodeURIComponent(url);
-    url = _APIURL_ + '?token=' + this.options.token + '&url=' + url;
-
-    for (const [key, value] of Object.entries(options)) {
-      if ('method' === key) {
-        continue;
-      }
-      url += '&' + snakeCase(key) + '=' + encodeURIComponent(value);
-    }
-
-    if (undefined === URL) { // Fix for older node versions
-      const urlModule = require('url');
-      return urlModule.parse(url);
-    }
-    return new URL(url);
   }
 
 }
